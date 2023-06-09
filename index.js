2e9b6e663d227b09ca69d454985fde3ea1472eb0
Ravindra Kumar Tilotia <ravindrakumartilotia4@gmail.com>
2023-06-09 12:28:14 +0530

    crudapp_backend

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { default: connection } = require("./Database/productDatabase");
 const { router } = require("./routes/allRoutes");
+const cors = require("cors");
+
 const app = express();
 app.use(express.json());
-app.use(router)
+app.use(cors());
+app.use(router);
+
 app.listen(8080, async (req, res) => {
   await connection;
   console.log("server is running");
