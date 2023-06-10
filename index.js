90c142fe5024194856372e5e1bbd640103cfd746
Ravindra Kumar Tilotia <ravindrakumartilotia4@gmail.com>
2023-06-10 07:42:25 +0530

    cors

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.listen(8080, async (req, res) => {
+const connectDB = async () => {
   await connection;
-  console.log("server is running");
+};
+connectDB().then(() => {
+  app.listen(8080, async (req, res) => {
+    // await connection;
+    console.log("server is running");
+  });
 });
